98c87758b16a75ea5b01fee646b75b2e6d013967
sidorela Kollcaku <sidorelakollcaku@gmail.com>
2020-06-18 21:47:40 -0400

    pushing the entire work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Work-Day-Scheduler
+ ALL ABOUT 
+
+ Track your day hourly . Input events and items so you don't forget to complete them for your day.
+
+Input events and items that for each hour of the day.
+​Automaticly add your events of what you have to do.
+
+Technologies Used
+​Javascript
+HTML5
+CSS
+
---
+++ app.js
@@ -0,0 +1,130 @@
+  
+$(document).ready(function(){
+
+    //Create body object
+    var body = $("body");
+
+    // Variables
+    var hour = moment().hour();
+
+    //Create all html assets
+    function createHtmlAsset(elTag, elId) {
+        var el = $(elTag);
+        el.attr("id", elId);
+        el.addClass(elId);
+        return el;
+    }
+
+    //Create Header
+    var head = createHtmlAsset("<header>", "header");
+    $(body).append(head);
+
+    //Create H1
+    var h1 = createHtmlAsset("<h1>", "plannerHead");
+    h1.text("Work Day Scheduler");
+    $(header).append(h1);
+
+    //Set interval to update the time by the second and add to header
+    var par = createHtmlAsset("<p>", "timerPar");
+    par.html(moment().format("ddd") + "</br>" + moment().format("LL") + "</br>" + moment().format("LTS"));
+    setInterval(function() {
+        par.html(moment().format("ddd") + "</br>" + moment().format("LL") + "</br>" + moment().format("LTS"));
+    }, 1000)
+    $(header).append(par);
+
+    //Create main div
+    var main = createHtmlAsset("<main>", "mainDiv");
+    $(body).append(main);
+
+    //Create time slots from 7am to 10pm
+    var calendarStartHour = 7;
+    var calendarEndHour = 22;
+    var time;
+    var amPM;
+
+    for (i = calendarStartHour; i < calendarEndHour + 1; i++) {
+        var calBlock = $("<div>");
+        var timeBlock = $("<div>");
+        var activityForm = $("<form>");
+        var activitiesBlock = $("<textarea>");
+        var saveBlock = $("<button>");
+
+        calBlock.addClass("calendarBlock");
+        calBlock.attr("id", "calendarBlock" + i);
+
+        timeBlock.addClass("timeBlock");
+        timeBlock.attr("id", "timeBlock" + i);
+
+        // Set am/pm
+        if (i > 12) {
+            time = i - 12;
+            amPM = "pm";
+        } else if (i < 12) {
+            time = i;
+            amPM = "am";                                                                                                                                              
+        } else {
+            time = i;
+            amPM = "pm";
+        }
+
+        timeBlock.text(time + " " + amPM);
+
+        activityForm.addClass("activityForm");
+        activityForm.attr("id", "activityForm" + i);
+        activityForm.attr("type", "text" + i);
+
+        activitiesBlock.addClass("activitiesBlock");
+        activitiesBlock.attr("id", "activitiesBlock" + i);
+        activitiesBlock.attr("time", i);
+        activitiesBlock.attr("placeholder", "Add events");
+
+        //Set colors
+        if (activitiesBlock.attr("time") == hour) {
+            activitiesBlock.addClass("now");
+        } else if (activitiesBlock.attr("time") < hour) {
+            activitiesBlock.addClass("past");
+        } else {}
+
+        // On new page load pull the appropriate value from localstorage and set it to the value of the textarea
+        activitiesBlock.val(localStorage.getItem("eventText" + i))
+
+        saveBlock.addClass("saveBlock");
+        saveBlock.attr("id", "saveBlock" + i);
+        saveBlock.attr("type", "submit");
+        saveBlock.html('<img src="./assets/floppy.png" />');
+
+        $(calBlock).append(timeBlock);
+        $(activityForm).append(activitiesBlock);
+        $(activityForm).append(saveBlock);
+        $(calBlock).append(activityForm);
+        $(main).append(calBlock);
+
+    }
+
+    //Map over each activitiesBlock and set background color according to time
+    // Set to interval so this will automatically happen without having to refresh the page
+    setInterval (function() {
+        $(".activitiesBlock").map(function(i) {
+            hour = moment().hour();
+            
+            if ($(this).attr("time") == hour) {
+                $(this).addClass("now");
+            } else if ($(this).attr("time") < hour) {
+                $(this).addClass("past");
+            } else {
+                $(this).removeClass("now");
+                $(this).removeClass("past");
+            }
+        })
+    }, 1000)
+
+    //Save Button on click event
+    var saveBtn = $(".saveBlock");
+    saveBtn.on("click", function (ev) {
+        ev.preventDefault();
+        var thisTime = $(this).parent().find("textarea").attr("time");
+        var inputText = $(this).parent().find("textarea").val();
+        var lsKeyName = "eventText" + thisTime;
+        localStorage.setItem(lsKeyName, inputText);
+    });
+})
